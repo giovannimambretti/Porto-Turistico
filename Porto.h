b560ba072c39ba9a0d85168bc2f674332f937d2f
giovannimambretti <79504756+giovannimambretti@users.noreply.github.com>
2021-05-07 16:04:44 +0200

    Update Porto.h

--- Porto.h
+++ Porto.h
@@ -1,32 +1,33 @@
 
 #pragma once
 #include "Barca.h"
+#include <string>
 
 using namespace std;
 
 class Porto
 {
- private:
- Barca barche[100];
- bool occupato[100];
+private:
+   
+    
 
-  float tariffa_vela;
-  float tariffa_motore;
+    float tariffa_vela;
+    float tariffa_motore;
 
- public:
+public:
 
- Porto(void);
- 
- int arrivo(Barca barca);
+    Barca barche[100];
+    Porto(void);
+    bool occupato[100];
+    int arrivo(Barca barca);
 
- float partenza(int posto, int giorni);
+    float partenza(int posto, int giorni);
 
- Barca informazioni(int posto);
- int cerca(float minimo, float massimo, Barca imbarcazioni[]);
- void setTariffa_vela(float tv);
- void setTariffa_motore(float tm);
- float getTariffa_vela(void);
- float getTariffa_motore(void);
- Barca modifica(int posto, string _nome, string _nazione,float _lunghezza,float _stazza);
+    Barca informazioni(int posto);
+    int cerca(float minimo, float massimo, Barca imbarcazioni[]);
+    void setTariffa_vela(float tv);
+    void setTariffa_motore(float tm);
+    float getTariffa_vela(void);
+    float getTariffa_motore(void);
+    Barca modifica(int posto, string _nome, string _nazione, float _lunghezza, float _stazza);
 };
-
